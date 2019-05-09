93d6d5ddfe9c7319c6a6a82c99c1b2acd6bbce6f
Howard Liu <hwd.liu@gmail.com>
2019-05-09 11:36:23 +1000

    Comment approver demonstrating react components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Sam' 
+                    timeAgo = "Today at 2am" 
+                    commentText="shoob borks" 
+                    profileImage={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Alex' 
+                    timeAgo = "Today at 1am" 
+                    commentText="I am shoob" 
+                    profileImage={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    author='Jane' 
+                    timeAgo = "Today at 5am" 
+                    commentText="I like doggos" 
+                    profileImage={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
